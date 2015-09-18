3c88c6f8912059a473d062abe965fb64a384d14a
raeffu <raeffu@raeffu.ch>
2015-09-18 09:18:51 +0200

    empty html site

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <title>PHP Seite</title>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="description" content="PHP Webshop">
+    <meta name="keywords" content="Webshop PHP">
+    <meta name="author" content="Steven Cardini, Raphael Laubscher">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+    <script src="js/application.js" type="text/javascript"></script>
+  </head>
+
+  <body>
+    <div class="container">
+      dini mueter
+      <?php phpinfo(); ?>
+    </div>
+  </body>
+</html>
