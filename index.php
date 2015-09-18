052816a47a965a70b806d5d7545fb98c2faafc86
Steven Cardini <steven@cardini.ch>
2015-09-18 12:55:37 +0200

    adapted flexbox style for page layout

--- index.php
+++ index.php
@@ -85,36 +85,34 @@
       Updates
     </div>
 
-    <div class="row">
-      <div class="col-*-*"></div>
+    <div class="main">
+      <div class="page-content">
+        <p>
+          Phasellus molestie hendrerit dui, vel bibendum tortor rutrum a. Curabitur eget quam viverra risus pharetra imperdiet at nec purus. Etiam vulputate, ante eget tristique ultrices, mauris sapien laoreet metus, sed venenatis urna lacus non dui. Morbi molestie vel nisl eget dictum. In pretium metus dolor, in mollis mi porta quis. Pellentesque pulvinar elit sed purus ultrices, eu pulvinar odio sagittis. Donec molestie, magna sed imperdiet condimentum, nunc odio congue mi, eu luctus magna lectus in ante. Donec eu leo pulvinar, mattis orci sollicitudin, iaculis quam. Curabitur iaculis a ante ac hendrerit. Pellentesque ac erat vitae orci pellentesque hendrerit at eget nulla. Integer sodales pretium efficitur. Sed diam turpis, volutpat eget velit a, tincidunt semper erat. Quisque velit turpis, blandit quis feugiat vel, dictum vel turpis. Suspendisse urna velit, rhoncus eget accumsan sed, lobortis id est. Suspendisse elementum viverra tellus a eleifend. In hac habitasse platea dictumst.
+        </p>
+
+        <p>
+          Integer quis mollis arcu. In varius dolor et vehicula aliquet. Vestibulum rhoncus ligula id sapien condimentum, vel eleifend nisi pellentesque. Mauris porttitor dolor id posuere aliquam. Aenean congue, magna id pulvinar lobortis, erat quam volutpat enim, at fringilla lacus dui id turpis. Aenean id tempor justo. Cras euismod vehicula lectus ut vulputate. Ut lacus ante, hendrerit tempus erat quis, viverra tincidunt orci.
+        </p>
+
+        <p>
+          Fusce lacinia sem at enim semper, sit amet mattis nulla tempor. Suspendisse a augue sit amet diam placerat gravida a quis tellus. Aenean ut facilisis libero. Mauris fringilla maximus libero, in eleifend leo facilisis a. Morbi massa nisi, finibus non posuere nec, molestie id neque. Sed dolor nisi, luctus pulvinar dolor et, interdum pharetra nisl. Nullam id velit sodales urna dignissim interdum nec vel arcu.
+
+        </p>
+
+        <p>
+          Integer vel aliquam sapien. Sed pulvinar ligula eget massa tempor, non tincidunt velit blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc tincidunt tellus sed mi feugiat, vitae posuere sem euismod. Nam quam diam, laoreet nec molestie at, sodales at tortor. Vestibulum in mauris quis mi maximus auctor facilisis quis arcu. Nullam nec auctor ante.
+
+        </p>
+
+
+
+      </div>
+
+      <aside>
+        Aside
+      </aside>
     </div>
-
-    <div class="page-content">
-      <p>
-        Phasellus molestie hendrerit dui, vel bibendum tortor rutrum a. Curabitur eget quam viverra risus pharetra imperdiet at nec purus. Etiam vulputate, ante eget tristique ultrices, mauris sapien laoreet metus, sed venenatis urna lacus non dui. Morbi molestie vel nisl eget dictum. In pretium metus dolor, in mollis mi porta quis. Pellentesque pulvinar elit sed purus ultrices, eu pulvinar odio sagittis. Donec molestie, magna sed imperdiet condimentum, nunc odio congue mi, eu luctus magna lectus in ante. Donec eu leo pulvinar, mattis orci sollicitudin, iaculis quam. Curabitur iaculis a ante ac hendrerit. Pellentesque ac erat vitae orci pellentesque hendrerit at eget nulla. Integer sodales pretium efficitur. Sed diam turpis, volutpat eget velit a, tincidunt semper erat. Quisque velit turpis, blandit quis feugiat vel, dictum vel turpis. Suspendisse urna velit, rhoncus eget accumsan sed, lobortis id est. Suspendisse elementum viverra tellus a eleifend. In hac habitasse platea dictumst.
-      </p>
-
-      <p>
-        Integer quis mollis arcu. In varius dolor et vehicula aliquet. Vestibulum rhoncus ligula id sapien condimentum, vel eleifend nisi pellentesque. Mauris porttitor dolor id posuere aliquam. Aenean congue, magna id pulvinar lobortis, erat quam volutpat enim, at fringilla lacus dui id turpis. Aenean id tempor justo. Cras euismod vehicula lectus ut vulputate. Ut lacus ante, hendrerit tempus erat quis, viverra tincidunt orci.
-      </p>
-
-      <p>
-        Fusce lacinia sem at enim semper, sit amet mattis nulla tempor. Suspendisse a augue sit amet diam placerat gravida a quis tellus. Aenean ut facilisis libero. Mauris fringilla maximus libero, in eleifend leo facilisis a. Morbi massa nisi, finibus non posuere nec, molestie id neque. Sed dolor nisi, luctus pulvinar dolor et, interdum pharetra nisl. Nullam id velit sodales urna dignissim interdum nec vel arcu.
-
-      </p>
-
-      <p>
-        Integer vel aliquam sapien. Sed pulvinar ligula eget massa tempor, non tincidunt velit blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc tincidunt tellus sed mi feugiat, vitae posuere sem euismod. Nam quam diam, laoreet nec molestie at, sodales at tortor. Vestibulum in mauris quis mi maximus auctor facilisis quis arcu. Nullam nec auctor ante.
-
-      </p>
-
-
-
-    </div>
-
-    <aside>
-      Aside
-    </aside>
 
     <footer>
       This is the footer
