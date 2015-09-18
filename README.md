a316af7803ecdc27cdcf7807be9aa0cd5a490221
Steven Cardini <steven@cardini.ch>
2015-09-18 11:37:39 +0200

    added description

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # hiragana.ch
+## Short description
+hiragana.ch is a web application that enables the user to (1) practice the Japanese syllabaries hiragana and katakana and (2) study Japanese vocabulary according to different levels of difficulty. The user will be able to track his progesses.
+
+## Content
+* HiraganaTrainer:
+This module enables the user to practice his knowledge of the Japanese hiragana syllabary.
+
+* KatakanaTrainer:
+This module enables the user to practice his knowledge of the Japanese katakana syllabary.
+
+* WordTrainer:
+This module enables the user to practice his knowledge of Japanese vocabulary. The user can progress through different levels of difficulty.
+
+* Statistics:
+Each user can monitor his own progress in this module.
+
+* Administration:
+Page admins can administer users and add new words for WordTrainer in this section.
--- index.php
+++ index.php
@@ -1,29 +1,125 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
-    <meta charset="utf-8">
-    <title>PHP Seite</title>
+<head>
+  <meta charset="utf-8">
+  <title>PHP Seite</title>
 
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <meta name="description" content="PHP Webshop">
-    <meta name="keywords" content="hiragana katakana japanese PHP">
-    <meta name="author" content="Steven Cardini, Raphael Laubscher">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="description" content="PHP Webshop">
+  <meta name="keywords" content="hiragana katakana japanese PHP">
+  <meta name="author" content="Steven Cardini, Raphael Laubscher">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <link rel="stylesheet" href="lib/bootstrap-3.3.5-dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
-    <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
+  <link rel="stylesheet" href="lib/bootstrap-3.3.5-dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
 
-    <script src="lib/jquery-1.11.3.min.js" type="text/javascript"></script>
-    <script src="lib/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
-    <script src="js/application.js" type="text/javascript"></script>
-  </head>
+  <script src="lib/jquery-1.11.3.min.js" type="text/javascript"></script>
+  <script src="lib/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
+  <script src="js/application.js" type="text/javascript"></script>
+</head>
 
-  <body>
-    <div class="container">
-      dini mueter
-      <?php phpinfo(); ?>
+<body>
+  <div class="container">
+
+    <div class="banner">
+      This is the banner
     </div>
-  </body>
+
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Brand</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li>
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+          <form class="navbar-form navbar-left" role="search">
+            <div class="form-group">
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+            <button type="submit" class="btn btn-default">Submit</button>
+          </form>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Link</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li><a href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+    <div class="updates">
+      Updates
+    </div>
+
+    <div class="row">
+      <div class="col-*-*"></div>
+    </div>
+
+    <div class="page-content">
+      <p>
+        Phasellus molestie hendrerit dui, vel bibendum tortor rutrum a. Curabitur eget quam viverra risus pharetra imperdiet at nec purus. Etiam vulputate, ante eget tristique ultrices, mauris sapien laoreet metus, sed venenatis urna lacus non dui. Morbi molestie vel nisl eget dictum. In pretium metus dolor, in mollis mi porta quis. Pellentesque pulvinar elit sed purus ultrices, eu pulvinar odio sagittis. Donec molestie, magna sed imperdiet condimentum, nunc odio congue mi, eu luctus magna lectus in ante. Donec eu leo pulvinar, mattis orci sollicitudin, iaculis quam. Curabitur iaculis a ante ac hendrerit. Pellentesque ac erat vitae orci pellentesque hendrerit at eget nulla. Integer sodales pretium efficitur. Sed diam turpis, volutpat eget velit a, tincidunt semper erat. Quisque velit turpis, blandit quis feugiat vel, dictum vel turpis. Suspendisse urna velit, rhoncus eget accumsan sed, lobortis id est. Suspendisse elementum viverra tellus a eleifend. In hac habitasse platea dictumst.
+      </p>
+
+      <p>
+        Integer quis mollis arcu. In varius dolor et vehicula aliquet. Vestibulum rhoncus ligula id sapien condimentum, vel eleifend nisi pellentesque. Mauris porttitor dolor id posuere aliquam. Aenean congue, magna id pulvinar lobortis, erat quam volutpat enim, at fringilla lacus dui id turpis. Aenean id tempor justo. Cras euismod vehicula lectus ut vulputate. Ut lacus ante, hendrerit tempus erat quis, viverra tincidunt orci.
+      </p>
+
+      <p>
+        Fusce lacinia sem at enim semper, sit amet mattis nulla tempor. Suspendisse a augue sit amet diam placerat gravida a quis tellus. Aenean ut facilisis libero. Mauris fringilla maximus libero, in eleifend leo facilisis a. Morbi massa nisi, finibus non posuere nec, molestie id neque. Sed dolor nisi, luctus pulvinar dolor et, interdum pharetra nisl. Nullam id velit sodales urna dignissim interdum nec vel arcu.
+
+      </p>
+
+      <p>
+        Integer vel aliquam sapien. Sed pulvinar ligula eget massa tempor, non tincidunt velit blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc tincidunt tellus sed mi feugiat, vitae posuere sem euismod. Nam quam diam, laoreet nec molestie at, sodales at tortor. Vestibulum in mauris quis mi maximus auctor facilisis quis arcu. Nullam nec auctor ante.
+
+      </p>
+
+
+
+    </div>
+
+    <aside>
+      Aside
+    </aside>
+
+    <footer>
+      This is the footer
+    </footer>
+
+  </div>
+</body>
 </html>
