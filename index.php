a67487503378c0170f9b66d6a3f8a6347b76fa83
Steven Cardini <steven@cardini.ch>
2015-09-18 11:00:30 +0200

    added bootstrap

--- index.php
+++ index.php
@@ -7,15 +7,16 @@
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="description" content="PHP Webshop">
-    <meta name="keywords" content="Webshop PHP">
+    <meta name="keywords" content="hiragana katakana japanese PHP">
     <meta name="author" content="Steven Cardini, Raphael Laubscher">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
-    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="lib/bootstrap-3.3.5-dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" type="text/css">
     <link rel="stylesheet" type="text/css" href="css/style.css" media="screen">
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+    <script src="lib/jquery-1.11.3.min.js" type="text/javascript"></script>
+    <script src="lib/bootstrap-3.3.5-dist/js/bootstrap.min.js"></script>
     <script src="js/application.js" type="text/javascript"></script>
   </head>
 
