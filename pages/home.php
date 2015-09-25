0ab2be059532d5d9d3e7b7807c580fa139e0b3e9
raeffu <raeffu@raeffu.ch>
2015-09-25 15:36:11 +0200

    Use umlauts

--- pages/home.php
+++ pages/home.php
@@ -1,4 +1,4 @@
-<h1>Überschrift</h1>
+<h1>&Uuml;berschrift</h1>
 
 <p>
   Phasellus molestie hendrerit dui, vel bibendum tortor rutrum a. Curabitur eget quam viverra risus pharetra imperdiet at nec purus. Etiam vulputate, ante eget tristique ultrices, mauris sapien laoreet metus, sed venenatis urna lacus non dui. Morbi molestie vel nisl eget dictum. In pretium metus dolor, in mollis mi porta quis. Pellentesque pulvinar elit sed purus ultrices, eu pulvinar odio sagittis. Donec molestie, magna sed imperdiet condimentum, nunc odio congue mi, eu luctus magna lectus in ante. Donec eu leo pulvinar, mattis orci sollicitudin, iaculis quam. Curabitur iaculis a ante ac hendrerit. Pellentesque ac erat vitae orci pellentesque hendrerit at eget nulla. Integer sodales pretium efficitur. Sed diam turpis, volutpat eget velit a, tincidunt semper erat. Quisque velit turpis, blandit quis feugiat vel, dictum vel turpis. Suspendisse urna velit, rhoncus eget accumsan sed, lobortis id est. Suspendisse elementum viverra tellus a eleifend. In hac habitasse platea dictumst.
