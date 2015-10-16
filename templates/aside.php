be93a05471011a4168861ee28c379bd85f0cab5d
Steven Cardini <steven@cardini.ch>
2015-10-16 10:12:01 +0200

    added login box

--- templates/aside.php
+++ templates/aside.php
@@ -1,4 +1,31 @@
+<?php
+  if (!isset($_SESSION['user']) || empty($_SESSION['user'])) { // user is logged in
+?>
+
+<aside>
+  <form class="form-signin">
+    <h2 class="form-signin-heading">Login</h2>
+    <label for="inputEmail" class="sr-only">E-Mail-Adresse</label>
+    <input type="email" id="inputEmail" class="form-control" placeholder="E-Mail-Adresse" required="" autofocus="">
+    <label for="inputPassword" class="sr-only">Passwort</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+    <div class="checkbox">
+      <label>
+        <input type="checkbox" value="remember-me"> Remember me
+      </label>
+    </div>
+    <button class="btn btn-default btn-block" type="submit">Sign in</button>
+  </form>
+</aside>
+
+<?php
+  } else { // user is not logged in
+?>
+
 <aside class="user-info">
   Welcome du User
   <img src="https://avatars3.githubusercontent.com/u/22323?v=3&s=460" />
 </aside>
+
+<?php
+  }
