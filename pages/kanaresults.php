abdc6fb1c11bde228158aa62be1d19c46abc3767
Steven Cardini <steven@cardini.ch>
2015-11-20 18:01:23 +0100

    display kanatrainer results on kanaresults page

--- pages/kanaresults.php
+++ pages/kanaresults.php
@@ -1,4 +1,4 @@
-<table class="table">
+<table id="kanaResultsTable" class="table">
   <thead>
     <tr>
       <th>Symbol</th>
@@ -8,35 +8,10 @@
   </thead>
 
   <tbody>
-<?php
-
-$statistics = array();
-  foreach ($statistics as $key => $row) {
-    if ($row['amountPrompted']<=0) {
-      continue;
-    }
-    $percentage = round ($row['amountCorrect'] / $row['amountPrompted'] * 100, 2);
-    if ($percentage>=80) { // set the color for the progress bars
-      $progressBarClass = 'progress-bar-success';
-    } elseif ($percentage<50) {
-      $progressBarClass = 'progress-bar-danger';
-    } else {
-      $progressBarClass = 'progress-bar-warning';
-    }
-
-
-?>
-      <tr>
-        <td><span class="h3"><?php echo $row['character']; ?></span> (<?php echo $key; ?>)</td>
-        <td><?php echo $row['amountCorrect'].' / '.$row['amountPrompted']; ?></td>
-        <td><div class="progress-bar <?php echo $progressBarClass; ?>" role="progressbar" aria-valuenow="<?php echo $percentage; ?>" aria-valuemin="0" aria-valuemax="100" style="width: <?php echo $percentage; ?>%;"><?php echo $percentage; ?>%</div></td>
-      </tr>
-  <?php
-} // end foreach
-   ?>
-    </tbody>
+        <?php // content is automatically added by kanaresults.pagecontrol.js ?>
+  </tbody>
  </table>
 
  <form method="post" action="">
-   <input type="submit" class="btn btn-info" name="back" value="Back">
+   <input type="submit" class="btn btn-info" name="back" value="Back" disabled="disabled">
  </form>
