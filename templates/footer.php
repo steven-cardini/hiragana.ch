8ab259448f0ac39d9aad609a181ecc8e29d6ecde
Steven Cardini <steven@cardini.ch>
2016-03-06 12:06:15 +0100

    refactored footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,9 +1,12 @@
-<footer>
-  <ul class='list-inline'>
+<footer class="navbar navbar-fixed-bottom">
+  <ul class='list-inline impressum'>
     <li>© 2015 - 2016</li>
     <li>|</li>
     <li><a href="<?php echo ROOT_DIR.'impressum'; ?>">Impressum</a></li>
-
-    <li><?php require_once(TEMPLATE_DIR.'lang_menu.php'); ?></li>
+  </ul>
+  <ul class="list-inline language">
+    <li><a href="#" data-lang="de">Deutsch</a></li>
+    <li>|</li>
+    <li><a href="#" data-lang="en">English</a></li>
   </ul>
 </footer>
--- templates/lang_menu.php
+++
@@ -1,5 +0,0 @@
-<ul class="list-inline js-language">
-  <li><a href="#" data-lang="de">Deutsch</a></li>
-  <li>|</li>
-  <li><a href="#" data-lang="en">English</a></li>
-</ul>
